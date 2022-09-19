c1d134c84157adea12b3baf2a0681074812e0d75
Annika Kangur <annika_kangur@hotmail.com>
2022-09-19 21:49:27 +0300

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Enter a Number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+
+int result = userNum % 2;
+
+//kui on paaris, siis on tulemus 0 ja on paaritu
+//== märk tähendab, et on võrdne
+//!= märk tähendab, et ei ole võrdne
+
+if (result != 0) 
+{
+    Console.WriteLine("User number is odd.");
+}else
+{
+    Console.WriteLine("User number is even.");
+}
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,10 @@
+﻿//modulus - % jääk, kui palju jääb peale jagamist üle
+
+int a = 24;
+int b = 8;
+
+int c = a % b; //+-*/
+//int b % a - väiksemat väärtust ei saa jagada suuremaga - vastuseks jääb see väärtus millest jagatakse (8 küpsist ei jagata 24-le inimesele. 8 küpsist jääb siis jagajajale)
+Console.WriteLine(c);
+
+
